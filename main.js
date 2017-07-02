73d361c560bdfb1b477141b93ffbe20679a61c75
ophir <ophir.elias@gmail.com>
2017-07-03 02:16:50 +0300

    uploading image

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fileUpload = require('express-fileupload');
 
 app.use("/api", express.static(path.join(__dirname, "public")));
-
+app.use(fileUpload());
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "/public/views/index.html"));
 });
 
+app.post('/photo', function(req, res) {
+  console.log("recieved a photo");
+})
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
 });
--- public/views/index.js
+++ public/views/index.js
@@ -1,4 +1,6 @@
 var app = angular.module('JsmileApp', ['ngRoute']);  
 app.controller('mainController', ['$scope', function($scope) {
-    
+    $scope.upload = function() {
+        $scope.img = document.getElementById("img").value;
+    }
 }]);
