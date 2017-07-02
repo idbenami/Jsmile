e1758199d52a4cd1012a5ccd692fe51d6791add4
User <user@Users-MacBook-Pro.local>
2017-07-03 01:33:10 +0300

    face plus plus api finish

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const faceReq = require('./app/faceReqHandler.js');
+
 const app = express();
 
 app.use("/api", express.static(path.join(__dirname, "public")));
@@ -8,6 +10,12 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "/public/views/index.html"));
 });
 
+app.get('/face', function (req, res) {
+    faceReq.faceDetect('idanPhoto.jpg', function(responseBody){
+        res.send(responseBody);
+    });
+});
+
 app.listen(3000, function () {
-  console.log('App listening on port 3000!')
+    console.log('App listening on port 3000!')
 });
